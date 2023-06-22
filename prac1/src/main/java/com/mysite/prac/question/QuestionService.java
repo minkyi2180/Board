3004b2e181ec4ad043f3cb7529527512bf17ffed
minkyi <minkyi2180@gmail.com>
2023-06-22 17:43:20 +0900

    3-02 완료

--- prac1/src/main/java/com/mysite/prac/question/QuestionService.java
+++ prac1/src/main/java/com/mysite/prac/question/QuestionService.java
@@ -1,9 +1,14 @@
 package com.mysite.prac.question;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.mysite.prac.DataNotFoundException;
@@ -34,5 +39,13 @@ public class QuestionService {
 		q.setContent(content);
 		q.setCreateDate(LocalDateTime.now());
 		this.questionRepository.save(q);
+	}
+	
+	
+	public Page<Question> getList(int page){
+		List<Sort.Order> sorts = new ArrayList<Sort.Order>();
+		sorts.add(Sort.Order.desc("createDate"));
+		Pageable pageable = PageRequest.of(page, 10,Sort.by(sorts));
+		return this.questionRepository.findAll(pageable);
 	}
 }
--- prac1/src/test/java/com/mysite/prac/PracApplicationTests.java
+++ prac1/src/test/java/com/mysite/prac/PracApplicationTests.java
@@ -8,26 +8,21 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import com.mysite.prac.question.Question;
 import com.mysite.prac.question.QuestionRepository;
+import com.mysite.prac.question.QuestionService;
 
 @SpringBootTest
 class PracApplicationTests {
 	
 	  @Autowired
-	    private QuestionRepository questionRepository;
+	    private QuestionService questionService;
 
 	    @Test
 	    void testJpa() {        
-	        Question q1 = new Question();
-	        q1.setSubject("sbb가 무엇인가요?");
-	        q1.setContent("sbb에 대해서 알고 싶습니다.");
-	        q1.setCreateDate(LocalDateTime.now());
-	        this.questionRepository.save(q1);  // 첫번째 질문 저장
-
-	        Question q2 = new Question();
-	        q2.setSubject("스프링부트 모델 질문입니다.");
-	        q2.setContent("id는 자동으로 생성되나요?");
-	        q2.setCreateDate(LocalDateTime.now());
-	        this.questionRepository.save(q2);  // 두번째 질문 저장
+	     for(int i =1; i<=100;i++) {
+	    	 String subject = String.format("테스트 데이터입니다:[%03d]", i);
+	    	 String content = "내용없음";
+	    	 this.questionService.create(subject, content);
+	     }
 	    }
 }
 	    
