5a908bae714acca757bcd2ae0234800dcbcda9e5
minkyi <minkyi2180@gmail.com>
2023-06-23 10:17:30 +0900

    3-07 로그인/로그아웃 완성

--- prac1/src/main/java/com/mysite/prac/user/UserController.java
+++ prac1/src/main/java/com/mysite/prac/user/UserController.java
@@ -2,6 +2,7 @@ package com.mysite.prac.user;
 
 import javax.validation.Valid;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -32,10 +33,27 @@ public class UserController {
                     "2개의 패스워드가 일치하지 않습니다.");
             return "signup_form";
         }
+        
+        try {
 
         userService.create(userCreateForm.getUsername(), 
                 userCreateForm.getEmail(), userCreateForm.getPassword1());
-
+        }catch(DataIntegrityViolationException e) {
+        	e.printStackTrace();
+        	bindingResult.reject("signupFailed", "이미 등록된 사용자입니다");
+        	return "signup_form";
+        }catch(Exception e) {
+        	e.printStackTrace();
+        	bindingResult.reject("signupFailed", e.getMessage());
+        	return "signup_form";
+        }
+        
         return "redirect:/";
+    }
+    
+    
+    @GetMapping("/login")
+    public String login() {
+    	return "login_form";
     }
 }
