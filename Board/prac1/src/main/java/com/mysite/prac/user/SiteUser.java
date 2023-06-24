577533c09bf7440240e19167afa9aee5398fb15f
minkyi2180 <130128767+minkyi2180@users.noreply.github.com>
2023-06-24 16:31:22 +0900

    Add files via upload
    게시판완성

---
+++ Board/prac1/src/main/java/com/mysite/prac/user/SiteUser.java
@@ -0,0 +1,27 @@
+package com.mysite.prac.user;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+public class SiteUser {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(unique = true)
+	private String username;
+	private String password;
+	
+	@Column(unique = true)
+	private String email;
+}
---
+++ Board/prac1/src/test/java/com/mysite/prac/PracApplicationTests.java
@@ -0,0 +1,28 @@
+package com.mysite.prac;
+
+import java.time.LocalDateTime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.mysite.prac.question.Question;
+import com.mysite.prac.question.QuestionRepository;
+import com.mysite.prac.question.QuestionService;
+
+@SpringBootTest
+class PracApplicationTests {
+	
+	  @Autowired
+	    private QuestionService questionService;
+
+	    @Test
+	    void testJpa() {        
+	    	for (int i = 1; i <= 10; i++) {
+	            String subject = String.format("테스트 데이터입니다:[%03d]", i);
+	            String content = "내용무";
+	            this.questionService.create(subject, content, null);
+	        }
+	    }
+}
+	    
